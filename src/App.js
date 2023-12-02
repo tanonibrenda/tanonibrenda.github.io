f5b54d4a6ac3288894fa89007db54e57056b7387
tanonibrenda <brendayohenatanoni@gmail.com>
2023-12-02 00:33:46 -0300

    router dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/home/Home'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <h1>Hola Brenda</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
